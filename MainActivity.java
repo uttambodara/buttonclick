95f9cedd5ca9427d7be6f2b1e968bc204753106d
hackerd <129719033+uttambodara@users.noreply.github.com>
2024-02-12 23:46:43 +0530

    MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.pr12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=(Button) findViewById(R.id.button_1id);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent Intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(Intent);
+
+            }
+        });
+
+    }
+}
+
